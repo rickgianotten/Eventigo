68c8023b9ca408173d20a92b1eeafee68076ad68
Rick206094 <206094@student.aventus.nl>
2026-05-01 11:01:51 +0200

    test: user cannot create event and added beforeach hook

--- Eventigo/tests/Feature/Event/CreateEventTest.php
+++ Eventigo/tests/Feature/Event/CreateEventTest.php
@@ -1,7 +1,29 @@
 <?php
 
-test('can create event',function(){})->todo();
+use App\Models\Event;
+use App\Models\PricingPlan;
+use App\Models\User;
+use Database\Factories\CompanyFactory;
+use Database\Seeders\PricingPlanSeeder;
+
+use function Pest\Laravel\actingAs;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+pest()->use(RefreshDatabase::class);
+
+beforeEach(function(){
+    $this->seed(PricingPlanSeeder::class);
+});
+
+test('user cannot create event',function(){
+    $user = User::factory()->create();
+
+    actingAs($user);
+
+    expect($user->can('create', Event::class))->toBeFalse();
+
+});
 
 test('can create unlimited events',function(){})->todo();
 
-test('can save event as concept',function(){})->todo();
+test('company can save event as concept',function(){})->todo();
