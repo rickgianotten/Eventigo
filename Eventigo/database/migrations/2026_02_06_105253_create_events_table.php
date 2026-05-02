76401db11ce5f97da9e5d4cbad099714e739d595
Rick206094 <206094@student.aventus.nl>
2026-05-02 14:54:53 +0200

    added status table to the migration

--- Eventigo/database/migrations/2026_02_06_105253_create_events_table.php
+++ Eventigo/database/migrations/2026_02_06_105253_create_events_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->enum('status', ['online', 'offline', 'concept'])->default('online');
             $table->foreignIdFor(Category::class)->constrained();
             $table->foreignIdFor(Company::class)->constrained()->cascadeOnDelete();   
             $table->string('title');         
