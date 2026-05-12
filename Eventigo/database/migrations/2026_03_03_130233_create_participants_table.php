a361127fe7a4a05e1e052fd9791b04932067efb2
Rick206094 <206094@student.aventus.nl>
2026-05-12 10:00:30 +0200

    feat: add nullable to name and email column

--- Eventigo/database/migrations/2026_03_03_130233_create_participants_table.php
+++ Eventigo/database/migrations/2026_03_03_130233_create_participants_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('participants', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name')->nullable();
+            $table->string('email')->nullable()->unique();
             $table->enum('role', ['artist', 'speaker', 'exhibitor', 'vendor']);
             $table->timestamps();
         });
