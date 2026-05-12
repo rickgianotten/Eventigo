2e4ab250ee4b8c3deec386ca651bc127b968d736
Rick206094 <206094@student.aventus.nl>
2026-05-12 10:41:10 +0200

    feat: removed unique from email column

--- Eventigo/database/migrations/2026_03_03_130233_create_participants_table.php
+++ Eventigo/database/migrations/2026_03_03_130233_create_participants_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('participants', function (Blueprint $table) {
             $table->id();
             $table->string('name')->nullable();
-            $table->string('email')->nullable()->unique();
+            $table->string('email')->nullable();
             $table->enum('role', ['artist', 'speaker', 'exhibitor', 'vendor']);
             $table->timestamps();
         });
