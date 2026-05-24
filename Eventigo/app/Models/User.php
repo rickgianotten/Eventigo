744ac39ab70229bb3bd7952db2a05024d21aa5bf
Rick206094 <206094@student.aventus.nl>
2026-05-24 14:57:15 +0200

    feat: add billable to user model

--- Eventigo/app/Models/User.php
+++ Eventigo/app/Models/User.php
@@ -8,11 +8,12 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Cashier\Billable;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, Billable;
 
     /**
      * The attributes that are mass assignable.
