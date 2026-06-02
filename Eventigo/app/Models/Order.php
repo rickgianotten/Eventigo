4f4644a0445bef661929fe6efe9beacb0166be17
Rick206094 <206094@student.aventus.nl>
2026-06-02 10:52:09 +0200

    feat: add fillable fields

--- Eventigo/app/Models/Order.php
+++ Eventigo/app/Models/Order.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    public $fillable = ['user_id', 'event_id','stripe_session_id', 'total_price', 'payment_status', 'paid_at'];
+
+    public function orderItems(){
+        return $this->hasMany(OrderItem::class);
+    }
 }
