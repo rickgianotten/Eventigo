23d28c1bb7f9b41ddf07211e8ff8b35862f59ef2
Rick206094 <206094@student.aventus.nl>
2026-06-02 10:30:06 +0200

    feat: add action for storing items in order items table

---
+++ Eventigo/app/Actions/Checkout/CreateOrderItemsAction.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Actions\Checkout;
+
+use App\Models\Order;
+use App\Models\Ticket;
+use Illuminate\Support\Collection;
+
+/**
+ * @param Ticket[] $lockedTickets
+ */
+
+class CreateOrderItemsAction{
+    public function handle(Order $order, Collection $lockedTickets, array $tickets ){
+
+        collect($tickets)->each(function($ticket) use ($order, $lockedTickets){
+        $lockedTicket = $lockedTickets[$ticket['ticket_id']];
+
+        // order items
+        $order->orderItems()->create([
+            'ticket_id' => $lockedTicket->id,
+            'quantity' => $ticket['quantity'],
+            'unit_price' => $lockedTicket->price
+        ]);
+        });
+    }
+}
