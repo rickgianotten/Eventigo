65232586ee150d6324b65da2a04f166cf7ee9ff6
Rick206094 <206094@student.aventus.nl>
2026-06-05 10:34:36 +0200

    feat: add OrderStatus enum to payment_status in casts function

--- Eventigo/app/Models/Order.php
+++ Eventigo/app/Models/Order.php
@@ -2,12 +2,16 @@
 
 namespace App\Models;
 
+use App\Enums\OrderStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Override;
 
 class Order extends Model
 {
+    use HasFactory;
     public $fillable = ['user_id', 'event_id','stripe_session_id', 'total_price', 'payment_status', 'paid_at'];
 
     public function orderItems():HasMany{
@@ -16,5 +20,12 @@ class Order extends Model
 
     public function event():BelongsTo{
         return $this->belongsTo(Event::class);
+    }
+
+    protected function casts():array
+    {
+        return [
+            'payment_status' => OrderStatus::class
+        ];
     }
 }
