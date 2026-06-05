4c636ee39d9d81d8dcf7cdb5051f5b209436874b
Rick206094 <206094@student.aventus.nl>
2026-06-05 09:58:59 +0200

    added event relationship and return type to orderItem relationship

--- Eventigo/app/Models/Order.php
+++ Eventigo/app/Models/Order.php
@@ -3,12 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
 {
     public $fillable = ['user_id', 'event_id','stripe_session_id', 'total_price', 'payment_status', 'paid_at'];
 
-    public function orderItems(){
+    public function orderItems():HasMany{
         return $this->hasMany(OrderItem::class);
+    }
+
+    public function event():BelongsTo{
+        return $this->belongsTo(Event::class);
     }
 }
