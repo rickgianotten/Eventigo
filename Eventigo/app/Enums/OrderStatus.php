b49e2d184e321f8944fbebcc80a4d4adfc63e924
Rick206094 <206094@student.aventus.nl>
2026-06-05 10:43:51 +0200

    feat: add random method for getting random order status

--- Eventigo/app/Enums/OrderStatus.php
+++ Eventigo/app/Enums/OrderStatus.php
@@ -7,4 +7,8 @@ enum OrderStatus: string
     case Pending = 'pending';
     case Paid = 'paid';
     case Cancelled = 'cancelled';
+
+    public static function random():self{
+        return fake()->randomElement(self::cases());
+    }
 }
