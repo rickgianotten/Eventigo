4aea94d9fbaf01363e8db94a2d314c5c30ca50bb
Rick206094 <206094@student.aventus.nl>
2026-06-09 09:23:08 +0200

    feat: add HasFactory to OrderItem model

--- Eventigo/app/Models/OrderItem.php
+++ Eventigo/app/Models/OrderItem.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class OrderItem extends Model
 {
+    use HasFactory;
     public $fillable = ['order_id', 'ticket_id', 'quantity', 'unit_price'];
+
+    public function tickets(): HasMany{
+        return $this->hasMany(OrderTicket::class);
+    }
 }
