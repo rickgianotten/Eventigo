48350052d51785421a6cabf313994818b0f229a1
Rick206094 <206094@student.aventus.nl>
2026-06-12 11:35:51 +0200

    feat: removed load for orderItems.tickets relationship

--- Eventigo/app/Actions/Checkout/CreateTicketsAction.php
+++ Eventigo/app/Actions/Checkout/CreateTicketsAction.php
@@ -7,7 +7,7 @@ use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 class CreateTicketsAction{
     public function handle(Order $order):array{
-        $tickets = $order->load('orderItems.tickets')->orderItems->flatMap(fn($item) => $item->tickets);
+        $tickets = $order->orderItems->flatMap(fn($item) => $item->tickets);
         return $tickets->map(fn($ticket) => QrCode::format('png')->generate($ticket->ticket_code))->toArray();
     }
 }
