708e50e568b9d29ced9725d2dff2d1ba74db233d
Rick206094 <206094@student.aventus.nl>
2026-06-23 12:02:44 +0200

    fix: adjust qr-code format to svg to prevent memory exhaustion

--- Eventigo/app/Actions/Checkout/CreateTicketsAction.php
+++ Eventigo/app/Actions/Checkout/CreateTicketsAction.php
@@ -8,6 +8,6 @@ use SimpleSoftwareIO\QrCode\Facades\QrCode;
 class CreateTicketsAction{
     public function handle(Order $order):array{
         $tickets = $order->orderItems->flatMap(fn($item) => $item->tickets);
-        return $tickets->map(fn($ticket) => QrCode::format('png')->generate($ticket->ticket_code))->toArray();
+        return $tickets->map(fn($ticket) => QrCode::format('svg')->generate($ticket->ticket_code))->toArray();
     }
 }
