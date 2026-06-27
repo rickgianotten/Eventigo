b59a10c63e24cae1740ed5bc9f458a4a566b572c
Rick206094 <206094@student.aventus.nl>
2026-06-27 14:12:16 +0200

    feat: add CheckoutController with store, succes and cancel methods

--- Eventigo/app/Http/Controllers/Checkout/CheckoutController.php
+++ Eventigo/app/Http/Controllers/Checkout/CheckoutController.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers\Checkout;
 
+use App\Actions\Checkout\CreateCheckoutAction;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateCheckoutRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
 
 class CheckoutController extends Controller
 {
-    public function create(Request $request){
-        dd($request->all());
+    public function store(CreateCheckoutRequest $request, CreateCheckoutAction $action){
+        $user = Auth::user();
+
+        return $action->handle($user, $request->validated('tickets'));
+    }
+
+    public function succes(Request $request): View{
+        return view('checkout.succes');
+    }
+
+    public function cancel(Request $request): View{
+        return view('checkout.cancel');
     }
 }
