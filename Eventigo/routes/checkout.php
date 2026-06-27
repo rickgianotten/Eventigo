457360592614a1e7b839adcab44b3831ebf1dbae
Rick206094 <206094@student.aventus.nl>
2026-06-27 14:24:03 +0200

    feat: add succes and cancel route and changed the '/checkout' route to '/'

--- Eventigo/routes/checkout.php
+++ Eventigo/routes/checkout.php
@@ -4,5 +4,8 @@ use App\Http\Controllers\Checkout\CheckoutController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function(){
-    Route::post('/checkout', [CheckoutController::class, 'store'])->name('store');
+    Route::post('/', [CheckoutController::class, 'store'])->name('store');
+
+    Route::get('/succes', [CheckoutController::class, 'succes'])->name('succes');
+    Route::get('/cancel', [CheckoutController::class, 'cancel'])->name('cancel');
 });
