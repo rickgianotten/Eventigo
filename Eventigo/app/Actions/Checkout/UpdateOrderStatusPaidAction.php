5b4dd4c2e9abc9a86c157670df85d721fbcf8bc9
Rick206094 <206094@student.aventus.nl>
2026-07-03 12:02:58 +0200

    feat: add action to update order status to 'paid'

---
+++ Eventigo/app/Actions/Checkout/UpdateOrderStatusPaidAction.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Actions\Checkout;
+
+use App\Enums\OrderStatus;
+use App\Models\Order;
+
+class UpdateOrderStatusPaidAction{
+    public function handle(Order $order){
+        $order->update([
+            'payment_status' => OrderStatus::Paid,
+            'paid_at' => now()
+        ]);
+    }
+}
