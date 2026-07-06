50d640204c320a9a143b308a2dd52f11123a2b52
Rick206094 <206094@student.aventus.nl>
2026-07-06 14:21:25 +0200

    feat: add case 'Failed' to OrderStatus enum

--- Eventigo/app/Enums/OrderStatus.php
+++ Eventigo/app/Enums/OrderStatus.php
@@ -7,6 +7,7 @@ enum OrderStatus: string
     case Pending = 'pending';
     case Paid = 'paid';
     case Cancelled = 'cancelled';
+    case Failed = 'failed';
 
     public static function random():self{
         return fake()->randomElement(self::cases());
