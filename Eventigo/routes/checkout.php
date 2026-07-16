a6b8af0974c01e9ba6c1b73a02d3dc64fc69adb9
Rick206094 <206094@student.aventus.nl>
2026-07-16 14:15:28 +0200

    feat: add 'EnsureCheckoutCompleted' middleware to succes and cancel route

--- Eventigo/routes/checkout.php
+++ Eventigo/routes/checkout.php
@@ -1,11 +1,12 @@
 <?php
 
 use App\Http\Controllers\Checkout\CheckoutController;
+use App\Http\Middleware\Checkout\EnsureCheckoutCompleted;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function(){
     Route::post('/', [CheckoutController::class, 'store'])->name('store');
 
-    Route::get('/succes', [CheckoutController::class, 'succes'])->name('succes');
-    Route::get('/cancel', [CheckoutController::class, 'cancel'])->name('cancel');
+    Route::get('/succes', [CheckoutController::class, 'succes'])->middleware(EnsureCheckoutCompleted::class)->name('succes');
+    Route::get('/cancel', [CheckoutController::class, 'cancel'])->middleware(EnsureCheckoutCompleted::class)->name('cancel');
 });
