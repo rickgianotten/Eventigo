2abaf243451de18e8755721758ddcd5a16877630
Rick206094 <206094@student.aventus.nl>
2026-07-23 13:50:12 +0200

    feat: add action to handle checkout session expired

---
+++ Eventigo/app/Actions/Checkout/HandleCheckoutExpiredAction.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Actions\Checkout;
+
+use App\Enums\OrderStatus;
+use App\Models\Order;
+
+class HandleCheckoutExpiredAction{
+    public function handle(array $object){
+        $metadata = $object['metadata'];
+        $order = Order::findOrFail($metadata['order_id']);
+
+        $order->update(['payment_status' => OrderStatus::Cancelled]);
+    }
+}
