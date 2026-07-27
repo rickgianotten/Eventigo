d1cc21e79868ebb00f9a32bd26f53b276c0f449c
Rick206094 <206094@student.aventus.nl>
2026-07-27 12:06:55 +0200

    fix: removed 'text-light-grey' class so that it doesn't overwrite

--- Eventigo/resources/views/components/icons/icon.blade.php
+++ Eventigo/resources/views/components/icons/icon.blade.php
@@ -1,1 +1,1 @@
-<span {{$attributes->merge(['class' => "material-symbols-outlined text-light-grey"])}}>{{$slot}}</span>
+<span {{$attributes->merge(['class' => "material-symbols-outlined"])}}>{{$slot}}</span>
