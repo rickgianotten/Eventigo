f8eca9d018b9bdaaf911250ebdee56dccbbe4bf4
Rick206094 <206094@student.aventus.nl>
2026-08-19 14:46:09 +0200

    feat: add PricingPlanSeeder, CategorySeeder, mocked HandleCheckoutCompletedAction, mocked HandleCheckoutExpiredAction and Order factory in beforeEach hook, afterEach hook with mockery close and the checkout session completed test

--- Eventigo/tests/Feature/Checkout/StripeWebhookListenerTest.php
+++ Eventigo/tests/Feature/Checkout/StripeWebhookListenerTest.php
@@ -1,30 +1,56 @@
 <?php
 
+use App\Actions\Checkout\HandleCheckoutCompletedAction;
+use App\Actions\Checkout\HandleCheckoutExpiredAction;
+use App\Listeners\Checkout\StripeWebhookListener;
 use App\Models\Event;
 use App\Models\Order;
 use App\Models\User;
+use Database\Seeders\CategorySeeder;
+use Database\Seeders\PricingPlanSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Laravel\Cashier\Events\WebhookReceived;
 
+pest()->use(RefreshDatabase::class);
+
 beforeEach(function(){
+    $this->seed([PricingPlanSeeder::class,CategorySeeder::class]);
     $this->user = User::factory()->create();
     $this->event = Event::factory()->create();
-    $this->order = Order::factory()->for($this->user)->create(['event_id' => $this->event_id]);
+    $this->order = Order::factory()->for($this->user)->create(['event_id' => $this->event->id]);
+    $this->mockedHandleCheckoutCompletedAction = $this->mock(HandleCheckoutCompletedAction::class);
+    $this->mockedHandleCheckoutExpiredAction = $this->mock(HandleCheckoutExpiredAction::class);
 });
 
-function makeWebhookReceivedEvent(string $type){
+afterEach(function(){
+    Mockery::close();
+});
+
+function makeWebhookReceivedEvent(string $type, Order $order){
     return new WebhookReceived([
     'type' => $type,
     'data' => [
         'object' =>[
             'metadata' =>[
-                'order_id' => $this->order->id
+                'order_id' => $order->id
             ]
         ]
     ]
     ]);
 }
 
-it('calls HandleCheckoutCompletedAction when checkout session is completed', function(){})->todo();
+it('calls HandleCheckoutCompletedAction when checkout session is completed', function(){
+    $webhookReceivedEvent = makeWebhookReceivedEvent('checkout.session.completed', $this->order);
+
+    $this->mockedHandleCheckoutCompletedAction->shouldReceive('handle')->once()->withArgs(function($object) use($webhookReceivedEvent){
+        return $object == $webhookReceivedEvent->payload['data']['object'];
+    });
+
+    $this->mockedHandleCheckoutExpiredAction->shouldNotReceive('handle');
+
+    app(StripeWebhookListener::class)->handle($webhookReceivedEvent);
+
+});
 
 it('calls HandleCheckoutExpiredAction when checkout session is expired', function(){})->todo();
 
