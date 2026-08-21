8177fc73ac8e5f39b4382417c2bc1a55155a9f2c
Rick206094 <206094@student.aventus.nl>
2026-08-21 14:37:24 +0200

    feat: add test for UpdateStockAction

---
+++ Eventigo/tests/Feature/Checkout/UpdateStockActionTest.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Actions\Checkout\UpdateStockAction;
+use App\Enums\Order\OrderStatus;
+use App\Models\Event;
+use App\Models\Order;
+use App\Models\OrderItem;
+use App\Models\Ticket;
+use App\Models\User;
+use Database\Seeders\CategorySeeder;
+use Database\Seeders\PricingPlanSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+pest()->use(RefreshDatabase::class);
+
+beforeEach(function(){
+    $this->seed([PricingPlanSeeder::class, CategorySeeder::class]);
+    $this->user = User::factory()->create();
+    $this->event = Event::factory()->create();
+    $this->ticket = Ticket::factory()->for($this->event)->create(['quantity_available' => '100', 'quantity_sold' => '0', 'price' => '200']);
+    $this->order = Order::factory()->for($this->user)->create(['event_id' => $this->event->id, 'payment_status' => OrderStatus::Paid]);
+    OrderItem::factory(2)->create(['order_id' => $this->order->id,'ticket_id' => $this->ticket->id, 'unit_price' => $this->ticket->price]);
+});
+
+it('increment the quantity_sold for each ticket', function(){
+    $totalExtraQuantity = $this->order->orderItems()->sum('quantity');
+
+    $previousQuantitySold = $this->ticket->quantity_sold;
+
+    app(UpdateStockAction::class)->handle($this->order);
+
+    expect($this->ticket->fresh()->quantity_sold)->toBe($previousQuantitySold + $totalExtraQuantity);
+
+});
