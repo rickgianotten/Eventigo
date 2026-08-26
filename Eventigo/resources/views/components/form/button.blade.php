9f7702a4523bf832a1dd3ab75fd1c682cb330600
Rick206094 <206094@student.aventus.nl>
2026-08-26 14:20:43 +0200

    feat: add blade disabled directive

--- Eventigo/resources/views/components/form/button.blade.php
+++ Eventigo/resources/views/components/form/button.blade.php
@@ -1,3 +1,3 @@
-<button {{$attributes->merge(['class' => 'flex items-center justify-center text-white rounded-md text-white p-2 cursor-pointer hover:opacity-75'])}}  type="submit" style="background: var(--gradient-button)">
+<button {{$attributes->merge(['class' => 'flex items-center justify-center text-white rounded-md text-white p-2 cursor-pointer hover:opacity-75'])}} @disabled($disabled ?? false) type="submit" style="background: var(--gradient-button)">
     {{$slot}}
 </button>
